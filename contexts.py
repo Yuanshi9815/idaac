7d7789d7457a8d69e6d8efbd2f580a7de84dbf86
Tan <yuanshi9812@outlook.com>
2023-01-07 12:06:16 +0800

    bossfight context

--- contexts.py
+++ contexts.py
@@ -32,6 +32,133 @@ contexts = {
             "boss_bullet_vel": 0.8,
             "max_extra_invulnerable": 1,
         },
-
+        "9":{
+            "boss_bullet_vel": 0.45,
+            "max_extra_invulnerable": 0,
+        },
+        "10":{
+            "boss_bullet_vel": 0.5,
+            "max_extra_invulnerable": 0,
+        },
+        "11":{
+            "boss_bullet_vel": 0.55,
+            "max_extra_invulnerable": 0,
+        },
+        "12":{
+            "boss_bullet_vel": 0.6,
+            "max_extra_invulnerable": 0,
+        },
+        "13":{
+            "boss_bullet_vel": 0.65,
+            "max_extra_invulnerable": 0,
+        },
+        "14":{
+            "boss_bullet_vel": 0.7,
+            "max_extra_invulnerable": 0,
+        },
+        "15":{
+            "boss_bullet_vel": 0.75,
+            "max_extra_invulnerable": 0,
+        },
+        "16":{
+            "boss_bullet_vel": 0.8,
+            "max_extra_invulnerable": 0,
+        },
+        "17":{
+            "boss_bullet_vel": 0.45,
+            "max_extra_invulnerable": 2,
+        },
+        "18":{
+            "boss_bullet_vel": 0.5,
+            "max_extra_invulnerable": 2,
+        },
+        "19":{
+            "boss_bullet_vel": 0.55,
+            "max_extra_invulnerable": 2,
+        },
+        "20":{
+            "boss_bullet_vel": 0.6,
+            "max_extra_invulnerable": 2,
+        },
+        "21":{
+            "boss_bullet_vel": 0.65,
+            "max_extra_invulnerable": 2,
+        },
+        "22":{
+            "boss_bullet_vel": 0.7,
+            "max_extra_invulnerable": 2,
+        },
+        "23":{
+            "boss_bullet_vel": 0.75,
+            "max_extra_invulnerable": 2,
+        },
+        "24":{
+            "boss_bullet_vel": 0.8,
+            "max_extra_invulnerable": 2,
+        },
+        "25":{
+            "boss_bullet_vel": 0.45,
+            "max_extra_invulnerable": 3,
+        },
+        "26":{
+            "boss_bullet_vel": 0.5,
+            "max_extra_invulnerable": 3,
+        },
+        "27":{
+            "boss_bullet_vel": 0.55,
+            "max_extra_invulnerable": 3,
+        },
+        "28":{
+            "boss_bullet_vel": 0.6,
+            "max_extra_invulnerable": 3,
+        },
+        "29":{
+            "boss_bullet_vel": 0.65,
+            "max_extra_invulnerable": 3,
+        },
+        "30":{
+            "boss_bullet_vel": 0.7,
+            "max_extra_invulnerable": 3,
+        },
+        "31":{
+            "boss_bullet_vel": 0.75,
+            "max_extra_invulnerable": 3,
+        },
+        "32":{
+            "boss_bullet_vel": 0.8,
+            "max_extra_invulnerable": 3,
+        },
+        "33":{
+            "boss_bullet_vel": 0.45,
+            "max_extra_invulnerable": 4,
+        },
+        "34":{
+            "boss_bullet_vel": 0.5,
+            "max_extra_invulnerable": 4,
+        },
+        "35":{
+            "boss_bullet_vel": 0.55,
+            "max_extra_invulnerable": 4,
+        },
+        "36":{
+            "boss_bullet_vel": 0.6,
+            "max_extra_invulnerable": 4,
+        },
+        "37":{
+            "boss_bullet_vel": 0.65,
+            "max_extra_invulnerable": 4,
+        },
+        "38":{
+            "boss_bullet_vel": 0.7,
+            "max_extra_invulnerable": 4,
+        },
+        "39":{
+            "boss_bullet_vel": 0.75,
+            "max_extra_invulnerable": 4,
+        },
+        "40":{
+            "boss_bullet_vel": 0.8,
+            "max_extra_invulnerable": 4,
+        },
     }
 }
