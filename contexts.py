fb35d456730363f15dec313ba2b0db4feaa3041c
yuanshi9815 <yuanshi9815@outlook.com>
2023-01-11 01:43:13 +0800

    update context

--- contexts.py
+++ contexts.py
@@ -160,6 +160,126 @@ contexts = {
             "boss_bullet_vel": 0.8,
             "max_extra_invulnerable": 4,
         },
+        "41": {
+            "boss_r": 3.0,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "42": {
+            "boss_r": 3.0,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "43": {
+            "boss_r": 3.0,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "44": {
+            "boss_r": 3.0,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        },
+        "45": {
+            "boss_r": 3.2,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "46": {
+            "boss_r": 3.2,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "47": {
+            "boss_r": 3.2,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "48": {
+            "boss_r": 3.2,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        },
+        "49": {
+            "boss_r": 3.4,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "50": {
+            "boss_r": 3.4,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "51": {
+            "boss_r": 3.4,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "52": {
+            "boss_r": 3.4,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        },
+        "53": {
+            "boss_r": 3.6,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "54": {
+            "boss_r": 3.6,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "55": {
+            "boss_r": 3.6,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "56": {
+            "boss_r": 3.6,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        },
+        "57": {
+            "boss_r": 3.8,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "58": {
+            "boss_r": 3.8,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "59": {
+            "boss_r": 3.8,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "60": {
+            "boss_r": 3.8,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        },
+        "61": {
+            "boss_r": 4.0,
+            "max_barriers_num": 3,
+            "min_barriers_num": 1
+        },
+        "62": {
+            "boss_r": 4.0,
+            "max_barriers_num": 4,
+            "min_barriers_num": 2
+        },
+        "63": {
+            "boss_r": 4.0,
+            "max_barriers_num": 5,
+            "min_barriers_num": 3
+        },
+        "64": {
+            "boss_r": 4.0,
+            "max_barriers_num": 6,
+            "min_barriers_num": 4
+        }    
     },
     "bigfish":{
         "1":{
